7343b2806b7f61bc84fc93d69ceea365aaafec15
Michael Smith <ketrimf@gmail.com>
2017-09-30 21:54:16 +0300

    Fixed driver loading to be compliant with JDBC 4

--- src/main/java/com/chtuqu/justhibernate/ch1/jdbc/JdbcConnectionManager.java
+++ src/main/java/com/chtuqu/justhibernate/ch1/jdbc/JdbcConnectionManager.java
@@ -6,7 +6,6 @@ public class JdbcConnectionManager {
     private static Connection connection;
 
     private static String url = "jdbc:mysql://localhost:3306/jh?useSSL=false";
-    private static String driverClass = "com.mysql.jdbc.Driver";
     private static String username = "idea";
     private static String password = "ideapass";
 
@@ -18,7 +17,6 @@ public class JdbcConnectionManager {
 
     private static void establishConnection() {
         try {
-            Class.forName(driverClass).newInstance();
             connection = DriverManager.getConnection(url, username, password);
         } catch (Exception ex) {
             ex.printStackTrace();
